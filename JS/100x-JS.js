fcfa7e69fb3eefa8697958937d6e25a5cf538087
Utpala9 <utpaladutta69@gmail.com>
2026-02-24 23:32:17 +0530

    100xDevs

---
+++ JS/100x-JS.js
@@ -0,0 +1,16 @@
+// Run js locally in cansole 
+// instal node js
+// write code on "runjs.aap/play"
+
+console.log("Hi There, What,s up");
+
+//Properties of JS : js is interprited language.
+
+ //Variables
+ let name = "Utpala" //Variable that can be reassigned
+ cont age = 30; // constant variable that cannot be reassigned
+ var isStudent = true;  //older way to declare vareaiables, function-scoped
+
+//Data Types
+let number = 42; //Number
+let string = "Hello World"; //Strng
